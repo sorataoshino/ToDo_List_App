bbcff0b1fabf2441eba2f009ccc7801a27a02686
sorataoshino <sorataoshino@gmail.com>
2025-04-03 21:21:11 +0200

    Ich glaub das isch de dritt Commit
    Schriib was du gmacht hesch, das isch die Idee vomne Commit... aa... aachhh... Und du machsch dir sorge, dass das Repository public isch...

--- src/components/card.js
+++ src/components/card.js
@@ -4,15 +4,16 @@ import Checkbox from "expo-checkbox"
 import {SIZES, FONTS, COLORS, SHADOW} from "../constants";
 
 const styles = StyleSheet.create({
-    view:{
+    view: {
         ...SHADOW,
         width: '100%',
-        paddingVertical: 12,
+        paddingVertical: 10,
         paddingHorizontal: SIZES.padding,
         borderRadius: SIZES.borderRadius,
         flexDirection: 'row',
         alignItems: 'center',
-        backgroundColor: COLORS.secondary
+        backgroundColor: COLORS.secondary,
+        marginBottom: 15
     },
     text: {
         ...FONTS.h2_semiBold,
@@ -28,10 +29,18 @@ const styles = StyleSheet.create({
 })
 
 export default function Card(props) {
-    const [isChecked, setChecked] = useState(false);
+    //const [isChecked, setChecked] = useState(false);
 
     return <View style={styles.view}>
-        <Checkbox style={styles.checkbox} value={isChecked} onValueChange={setChecked}/>
-        <Text style={styles.text}>{props.text}</Text>
+        <Checkbox 
+            style={styles.checkbox}
+            //value={isChecked} onValueChange={setChecked}
+            value={props.data.isSelected}
+            onValueChange={() => props.setIsSelected()}
+
+        />
+        <Text style={styles.text}>{props.data.text}</Text> 
     </View>
 }
+
+//Checkboxes need to be changed again, because our Styles-Dude uses an old component.
--- src/screens/Homepage.js
+++ src/screens/Homepage.js
@@ -1,49 +1,96 @@
-import React from "react";
-import { View, Text, StyleSheet, StatusBar, Platform, TextInput, TouchableOpacity} from "react-native";
-import { Card } from "../components";
-import { COLORS, SIZES, FONTS, SHADOW } from "../constants";
+import {React, useState } from 'react';
+import { View, Text, StyleSheet, StatusBar, Platform, TextInput, TouchableOpacity, FlatList, Alert } from "react-native"
+import { Card } from "../components"
+import { COLORS, SIZES, FONTS, SHADOW } from "../constants"
 
 const styles = StyleSheet.create({
     container: {
-        paddingTop: Platform.OS === 'ios' ? 40 : StatusBar.currentHeight + 10,
+        paddingTop: Platform.OS === "ios" ? 40 : StatusBar.currentHeight + 10,
         flex: 1,
         backgroundColor: COLORS.primary,
         padding: SIZES.padding
     },
-    textBoxWrapper:{
+    textBoxWrapper: {
+        width: "100%",
         position: "absolute",
         bottom: 0,
         left: 0,
         flexDirection: "row",
-        justifyContent: "space-between"
+        alignItems: "center",
+        justifyContent: "space-between",
+        padding: SIZES.padding
     },
-    textInput:{
+    textInput: {
         ...SHADOW,
         borderRadius: SIZES.textBoxRadius,
         backgroundColor: COLORS.secondary,
-        height: 54,
-        width: "80%",
+        height: 42,
+        paddingLeft: 15,
+        width: "90%",
         color: COLORS.primary,
+        marginRight: 15,
+        ...FONTS.h2_semiBold,
     },
-    btn:{
-        backgroundColor: "white",
-        height: 54,
-        width: "18%"
+    btn: {
+        ...SHADOW,
+        backgroundColor: COLORS.accent,
+        height: 42,
+        width: 42,
+        borderRadius: 100,
+        alignItems: "center",
+        justifyContent: "center",
     }
 })
 
 export default function Homepage(){
+
+    const [list, setList] = useState([]);
+    const [value, setValue] = useState("");
+
+    function addText(text){
+        if (value !== ""){
+            setList(prev => {
+                return [
+                    ...prev,
+                    {text: text, isSelected: false} 
+                ]
+            })
+            setValue("")
+        } else {
+            alert("Please type in something! uwu <3")
+        }
+    }
+
+    function setIsSelected(index, value){
+        let data = []
+        
+        for (let i = 0; i < list.length; i++) {
+            if (index === i){
+                data.push({...list[i], isSelected: value})
+            } else {
+                data.push(list[i])
+            }
+        }
+
+        setList(data);
+    }
+
     return <View style ={styles.container}>
-        <Card text={"Go to signing class"} />
+        <Text style={{...FONTS.h1_semiBold, color: COLORS.secondary, marginbottom: 15}}>What need to be done.</Text>
+        <FlatList style={{flex: 1}}
+            data={list}
+            renderItem={({item, index}) => <Card data={item} index={index} setIsSelected={setIsSelected} />}
+            keyExtractor={(item, index) => index.toString()}
+
+        />
         <View style={styles.textBoxWrapper}>
-            <TextInput style={styles.textInput} />
-            <TouchableOpacity style={styles.btn}>
-                <Text>+</Text>
+            <TextInput style={styles.textInput} placeholder="New Task" placeholderTextColor = {COLORS.primary} onChangeText={text => setValue(text)} value={value}/>
+            <TouchableOpacity style={styles.btn} onPress={() => addText(value)}>
+                <Text style={{fontSize: 34, color: COLORS.secondary}}>+</Text>
             </TouchableOpacity>
         </View>
     </View>
 }
 
-
-// https://www.youtube.com/watch?v=SsMHZtii0w4
-// 53:21
+// https://youtu.be/SsMHZtii0w4?si=93uwz5jidSUi584_&t=5700
+// 1:35:00
